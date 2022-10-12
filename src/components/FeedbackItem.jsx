f7b1527d472c92244d279c91696d0c17e46bfd88
Koko-Dev <koko.webdev@gmail.com>
2022-10-11 20:23:17 -0400

    FeedbackItem.jsx:  Add a temporary button which when clicked sets the rating to ten

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,12 +5,17 @@ function FeedbackItem () {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of a feedback item.');
 
+  const handleClick = () => {
+    setRating(10);
+  };
+
   return (
     <div className='card'>
       <div className='num-display'>{ rating }</div>
       {/* /.num-display */ }
       <div className='text-display'>{ text }</div>
       {/* /.text-display */ }
+      <button onClick={ handleClick }>Click</button>
     </div>
   );
 }
