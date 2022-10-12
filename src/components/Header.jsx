6ed3e5d4ffea1fe2c6196ac503a714e5c6fe970b
Koko-Dev <koko.webdev@gmail.com>
2022-10-11 20:00:00 -0400

    App.js:  Remove Header props;  Header.jsx:  Add styling variables as properties to defaultProps

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,6 +21,8 @@ function Header ({ text, bgColor, textColor }) {
 
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
 };
 
 Header.propTypes = {
