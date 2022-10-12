abe46425438f4aff5c0df371cb5511d6374b10fd
Koko-Dev <koko.webdev@gmail.com>
2022-10-11 20:18:18 -0400

    FeedbackItem.jsx:  Add states for rating and text and replace hardcoded data to these states in return div

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
 
 function FeedbackItem () {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState('This is an example of a feedback item.');
+
   return (
     <div className='card'>
-      <div className='num-display'>10</div>
-      {/* /.num-display */}
-      <div className='text-display'>
-        This is an example of a feedback item.
-      </div>
-      {/* /.text-display */}
+      <div className='num-display'>{ rating }</div>
+      {/* /.num-display */ }
+      <div className='text-display'>{ text }</div>
+      {/* /.text-display */ }
     </div>
   );
 }
+
 
 export default FeedbackItem;
