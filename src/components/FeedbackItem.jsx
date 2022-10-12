31f61801ac38a97c729127aa87d50ad433e087a1
Koko-Dev <koko.webdev@gmail.com>
2022-10-11 20:40:24 -0400

    App.js: Import FeedbackData and useState; create feedback state and set to FeebackData;  FeedbackItem.jsx:  Remove button and click handler

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,20 +5,12 @@ function FeedbackItem () {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of a feedback item.');
 
-  const handleClick = () => {
-    setRating((prevRating) => {
-      console.log(prevRating);
-      return prevRating + 1;
-    });
-  };
-
   return (
     <div className='card'>
       <div className='num-display'>{ rating }</div>
       {/* /.num-display */ }
       <div className='text-display'>{ text }</div>
       {/* /.text-display */ }
-      <button onClick={ handleClick }>Click</button>
     </div>
   );
 }
