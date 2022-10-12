97b92328331f7149ce9deef75a4c70e3073c346c
Koko-Dev <koko.webdev@gmail.com>
2022-10-12 19:04:35 -0400

    FeedbackItem.jsx:  Refactor

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,6 +1,7 @@
 import Card from './shared/Card';
 
-function FeedbackItem ({item}) {
+
+function FeedbackItem ({ item }) {
   return (
     <Card>
       <div className='num-display'>{ item.rating }</div>
