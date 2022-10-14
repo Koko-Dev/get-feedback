8a03c49a959d9427530947c34bbd72a203a4cbab
Koko-Dev <koko.webdev@gmail.com>
2022-10-13 22:34:20 -0400

    RatingSelect.jsx:  Remove import of useState and console log;  Refactor

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react';
-
-
 function RatingSelect ({ select, selected }) {
   const handleChange = (e) => {
     select(+e.currentTarget.value);
-    console.log(+e.currentTarget.value)
   };
 
   return (
