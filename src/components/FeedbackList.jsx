701ba569f6c34894c03a9ce9c4f9cc8f1a28443f
Koko-Dev <koko.webdev@gmail.com>
2022-10-16 15:07:31 -0400

    FeedbackList.jsx:  Import motion and AnimatePresence from framer-motion;  surround the return feedback mapping with the AnimatePresence component; wrap the <motion.div> component around the call to <FeedbackItem /> along with the transformation we desire when we create a post/remove a feedback item

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,7 @@
-import FeedbackItem from './FeedbackItem'
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
+
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
@@ -10,6 +12,29 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
+      <AnimatePresence>
+        { feedback.map(item => (
+          <motion.div
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <FeedbackItem
+              key={ item.id }
+              item={ item }
+              handleDelete={ handleDelete }
+            />
+
+          </motion.div>
+        )) }
+      </AnimatePresence>
+    </div>
+  )
+}
+
+  /*return (
+    <div className="feedback-list">
       { feedback.map(item => (
         <FeedbackItem
           key={ item.id }
@@ -19,7 +44,7 @@ function FeedbackList({ feedback, handleDelete }) {
       )) }
     </div>
   )
-}
+}*/
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
