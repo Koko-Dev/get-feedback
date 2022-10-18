0d2ec527f6854f72671efc0ccc14db2a54ad1437
Koko-Dev <koko.webdev@gmail.com>
2022-10-18 10:05:20 -0400

    Create a homepage route and an about page route; the about page route should have a link returning it back to the homepage where the feedback form, stats and list is displayed;  modify the about page to display as a card just like the homepage components are displayed
    1.  NPM install react-router-dom;
    
    2.  App.js:  Import BrowserRouter as Router, Routes and Route from react-router-dom
    
    3.  Create routes for '/' and '/about' pages
    
    4.  AboutPage.jsx:  Modify and create a link that returns the client back to the homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -25,18 +26,30 @@ function App() {
 
 
   return (
-    <>
+    <Router>
       <Header/>
       <div className="container">
-        <FeedbackForm handleAdd={ addFeedback }/>
-        <FeedbackStats feedback={ feedback }/>
-        <FeedbackList
-          feedback={ feedback }
-          handleDelete={ deleteFeedback }
-        />
-        <AboutPage />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={ addFeedback }/>
+                <FeedbackStats feedback={ feedback }/>
+                <FeedbackList
+                  feedback={ feedback }
+                  handleDelete={ deleteFeedback }
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={ <AboutPage/> }/>
+        </Routes>
+
+
       </div>
-    </>
+    </Router>
   )
 }
 
