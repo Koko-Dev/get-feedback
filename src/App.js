ee1474ef0c4ba8b45a3c01048cd5788d4a8e5752
Koko-Dev <koko.webdev@gmail.com>
2022-10-18 13:01:05 -0400

    App.js: Since we are no longer receiving feedback data as a prop to FeedbackList, remove feedback prop from its return

--- src/App.js
+++ src/App.js
@@ -40,9 +40,7 @@ function App() {
                 <>
                   <FeedbackForm handleAdd={ addFeedback }/>
                   <FeedbackStats feedback={ feedback }/>
-                  <FeedbackList
-                    feedback={ feedback }
-                    handleDelete={ deleteFeedback }
+                  <FeedbackList handleDelete={ deleteFeedback }
                   />
                 </>
               }
