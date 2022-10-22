03746054437f27535a91e2059a09e9c2c67ae21c
Koko-Dev <koko.webdev@gmail.com>
2022-10-22 13:06:03 -0400

    Enable any new feedback post submitted to persist to db.json and continue to display on refresh
    FeedbackContext.js:
       1.  Remove import of v4 as uuidv4 from uuid;
       2.  In FeedbackProvider({children}), in addFeedback(newFeedback):
             a.  Make function async
             b.  Since package.json has proxy set at http://localhost:5000, we no longer need to add that portion to the url for fetch input; make fetch input '/feedback' and fetch init method POST with headers content-type set to application/json and body set to JSON.stringify(newFeedback)
             c.  setFeedback to [ data, ...feedback ]

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,10 +1,9 @@
 import {createContext, useEffect, useState} from 'react'
-import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [ isLoading, setIsLoading ] = useState(true)
   const [ feedback, setFeedback ] = useState([])
   const [ feedbackEdit, setFeedbackEdit ] = useState({
                                                        item: {},
@@ -25,10 +24,19 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([ ...feedback, newFeedback ])
-    console.log(feedback)
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(
+      '/feedback',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newFeedback),
+      })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
 
   // Delete feedback
